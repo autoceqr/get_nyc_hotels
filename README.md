e487eeb673762a195ddb8ca14bd20c4545fda8f3
DanielMSheehan <danielmsheehan@Daniels-MBP-13.lan>
2019-10-24 13:47:39 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Get NYC Hotels Data
 Repo for extracting NYC hotel data from NYC Go
+
+
+```
+from nyc_hotel_data import get_nyc_go_hotels_data
+from datetime import datetime
+
+hotel_id_list = [ ...list of hotel ids... ] 
+
+dt_string = now.strftime("%Y-%d-%m_%H-%M-%S")
+
+get_nyc_go_hotels_data(
+    hotel_id_list, 
+    'data/nyc_go_hotels_{}.csv'.format(dt_string)
+)
+```
---
+++ nyc_hotel_data.py
@@ -0,0 +1,51 @@
+import requests
+import pandas as pd
+import time
+from io import BytesIO
+
+
+def get_nyc_go_hotel_json(hotel_id):
+    return requests.get(
+        "https://api.nycgo.com/api/v2/hotels?hotelId={}&limit=1".format(
+            hotel_id,
+        )
+    ).json()
+
+
+def extract_response_data(data):
+    return {
+        'hotel_id': data['data'][0]['hotelId'],
+        'longitude': data['data'][0]['hotelData']['location']['longitude'],
+        'latitude': data['data'][0]['hotelData']['location']['latitude'],
+        'num_rooms': data['data'][0]['hotelData']['numberOfRooms'],
+        'city': data['data'][0]['hotelData']['city'],
+        'zip_code': data['data'][0]['hotelData']['zip'],
+        'hotel_type_id': data['data'][0]['hotelData']['hotelTypeId'],
+        'city_id': data['data'][0]['hotelData']['cityId'],
+        'address': data['data'][0]['hotelData']['address'],
+        'hotel_class': data['data'][0]['hotelData']['class'],
+        'name': data['data'][0]['hotelData']['name'],
+        'district_id': data['data'][0]['hotelData']['districtId'],
+        'url': data['data'][0]['hotelData']['url']
+    }
+
+
+def get_nyc_go_hotels_data(
+        hotel_id_list,
+        output_csv,
+):
+    hotel_data = {}
+
+    for hotel_id in hotel_id_list:
+        hotel_data[hotel_id] = extract_response_data(
+            get_nyc_go_hotel_json(hotel_id)
+        )
+        time.sleep(1)
+
+    pd.DataFrame(hotel_data).T.to_csv(output_csv, index=False)
+
+
+def google_sheet_to_dataframe(url):
+    r = requests.get(url)
+    data = r.content
+    return pd.read_csv(BytesIO(data))
